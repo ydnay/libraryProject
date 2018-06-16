54ff22395bb94ada42180d70e098cd568a63f9d0
Alan Cardero <alan.cardero@gmail.com>
2018-06-16 10:52:55 -0400

    Adding the whole code used on the  lesson

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Book = require('../models/book.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then(books => {
+      console.log(books);
+      res.render("books", { books });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+router.get('/book/:id', (req, res, next) => {
+  let bookId = req.params.id;
+  Book.findOne({'_id': bookId})
+    .then(book => {
+      res.render("book-detail", { book })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
